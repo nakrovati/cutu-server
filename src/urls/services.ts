3f197a18f424fef2476dda96ca11144fb103284a
Daniil Zhuravlev <nakrovati@outlook.com>
2024-02-21 00:13:46 +0400

    perf: add lru-cache for resoling original url route

--- src/urls/services.ts
+++ src/urls/services.ts
@@ -1,4 +1,5 @@
 import { type NewShortenedUrl } from "@/config/db/schema.js";
+import { LRUCache } from "lru-cache";
 import {
   findOriginUrlByShortCode,
   findShortenedUrlByShortCode,
@@ -7,6 +8,9 @@ import {
 import { generateShortCode } from "./utils/createShortCode.js";
 
 const { BACKEND_URL } = Bun.env;
+const cache = new LRUCache<string, string>({
+  max: 500,
+});
 
 export async function getShortenedUrl(shortCode: string) {
   const shortenedUrl = await findShortenedUrlByShortCode.execute({
@@ -25,11 +29,17 @@ export async function getShortenedUrl(shortCode: string) {
 }
 
 export async function getOriginalUrl(shortCode: string) {
+  if (cache.has(shortCode)) {
+    return cache.get(shortCode);
+  }
+
   const shortenedUrl = await findOriginUrlByShortCode.execute({
     shortCode,
   });
 
   if (shortenedUrl.length === 0) return undefined;
+
+  cache.set(shortCode, shortenedUrl[0].originalUrl);
 
   return shortenedUrl[0].originalUrl;
 }
